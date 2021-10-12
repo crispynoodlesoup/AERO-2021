c1de7c381917c1d072b352a00f14044abf0d1c94
crispynoodlesoup <51384880+crispynoodlesoup@users.noreply.github.com>
2021-10-12 10:52:56 -0500

    Update ColorAuto_Blue.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorAuto_Blue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorAuto_Blue.java
@@ -9,9 +9,9 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
  */
 @Autonomous(name="SkystoneBlue", group="generated")
 public class ColorAuto_Blue extends LinearOpMode {
-    // Objects and variables
+    // Objects and variables from our other custom classes
     ControlCalc calc = new ControlCalc();
-    HardwareMecanumbot robot = new HardwareMecanumbot();
+    HardwareMecanumbot robot = new HardwareMecanumbot();\
     
     // Code that runs when the driver initializes
     @Override
@@ -21,15 +21,51 @@ public class ColorAuto_Blue extends LinearOpMode {
         
         // Code that runs when the driver presses PLAY
         waitForStart();
+        
+        /* First, we'll be driving by time
+         * This is the most basic auto you can make
+         * It is not that accurate, but it usually gets the job done
+         */
+        
+        // reset runtime, or measured time, to 0
         robot.runtime.reset();
-        while( !isStopRequested() ) {
-            calc.calcTurnPower(0);
-            robot.updateEncoderValues();
-
-            telemetry.addData("encoder ticks per rev", robot.encoderFL);
-            telemetry.addData("milliseconds delta", calc.milliseconds - robot.runtime.milliseconds());
+        
+        // check code should still be running, and drive forward until it's been more than one second
+        // using the custom function moveMecanum written in HardwardMecanumbot file
+        // set forward drive speed to 0.8
+        robot.moveMecanum(0.8, 0, 0, 0);
+        while(opModeIsActive() && !isStopRequested() && robot.runtime.seconds() < 1.0) {
+            // telemetry writes data you give it to the driver station (phone) in real time
+            // while waiting for this while loop to end, 
+            telemetry.addData("Path", "Split 1: %2.5f S Elapsed", robot.runtime.seconds());
             telemetry.update();
         }
-        robot.moveMecanum(0,0,0,0); // Stop the robot
+        robot.moveMecanum(0, 0, 0, 0); // stop robot
+
+        // reset runtime, or measured time, to 0
+        robot.runtime.reset();
+        
+        // check code should still be running, and turn until it's been more than one second
+        robot.moveMecanum(0, 0, 0.5, 0);
+        while(opModeIsActive() && !isStopRequested() && robot.runtime.seconds() < 1.0) {
+            telemetry.addData("Path", "Split 2: %2.5f S Elapsed", robot.runtime.seconds());
+            telemetry.update();
+        }
+        robot.moveMecanum(0, 0, 0, 0); // stop robot
+
+        // reset runtime, or measured time, to 0
+        robot.runtime.reset();
+        
+        // check code should still be running, and strafe until it's been more than one second
+        robot.moveMecanum(0, 0.8, 0, 0);
+        while(opModeIsActive() && !isStopRequested() && robot.runtime.seconds() < 1.0) {
+            telemetry.addData("Path", "Split 3: %2.5f S Elapsed", robot.runtime.seconds());
+            telemetry.update();
+        }
+        robot.moveMecanum(0,0,0,0); // make sure robot is stopped
+        
+        telemetry.addData("Drive by time path", "complete");
+        telemetry.update();
+        sleep(3000);
     }
 }
